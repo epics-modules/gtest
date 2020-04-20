794bfc16babed4f40a63d0e9765bb12898dd8d22
Ralph Lange <ralph.lange@gmx.de>
2020-04-20 11:42:27 +0200

    Make tap output configurable (--gtest_output_tap)

--- gtestSup/gtest_main.cc
+++ gtestSup/gtest_main.cc
@@ -1,36 +1,36 @@
+/*************************************************************************\
+* Copyright (c) 2020 ITER Organization.
+* This module is distributed subject to a Software License Agreement found
+* in file LICENSE that is included with this distribution.
+\*************************************************************************/
+
 /*
- * Copyright (c) <2011> <Bruno P. Kinoshita - http://www.kinoshita.eti.br>.
- *
- * The MIT License
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ *  Author: Ralph Lange <ralph.lange@gmx.de>
  */
 
 #include "gtest/gtest.h"
 #include "tap.h"
 
 GTEST_API_ int main(int argc, char **argv) {
+    bool addTapListener = false;
+
+    if (argc > 1) {
+        if (!strcmp(argv[1], "--gtest_output_tap")) {
+            addTapListener = true;
+            // Shift the remainder of the argv list left by one, including the trailing NULL
+            for (int j = 1; j != argc; j++) {
+              argv[j] = argv[j + 1];
+            }
+            argc--;
+        }
+    }
+
     testing::InitGoogleTest(&argc, argv);
 
-    testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
-    listeners.Append(new tap::TapListener());
-
+    if (addTapListener) {
+        testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+        listeners.Append(new tap::TapListener());
+    }
     return RUN_ALL_TESTS();
 }
 
